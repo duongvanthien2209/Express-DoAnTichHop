d6e5911acde45c239932024fe9427ddd2bd5dc86
unknown <duongthien2209@gmail.com>
2021-01-21 20:30:55 +0700

    change register user -1

--- controllers/user/auth.controller.js
+++ controllers/user/auth.controller.js
@@ -1,15 +1,15 @@
 /* eslint-disable no-underscore-dangle */
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const crypto = require('crypto');
+// const crypto = require('crypto');
 // const path = require('path');
 const { validationResult } = require('express-validator');
 
 // Models
 const User = require('../../models/User');
-const Token = require('../../models/Token');
+// const Token = require('../../models/Token');
 
-const sendEmail = require('../../utils/sendEmail');
+// const sendEmail = require('../../utils/sendEmail');
 const Response = require('../../helpers/response.helper');
 
 const limit = 20;
@@ -175,32 +175,33 @@ exports.register = async (req, res, next) => {
         parseInt(dateParts[1], 10) - 1,
         parseInt(dateParts[0], 10),
       ),
+      isVerified: true,
       // ngaySinh,
     });
 
-    // Tạo 1 token -> lưu lại -> gởi email + token -> email gởi lại token hợp lệ -> verified user
-    const token = crypto.randomBytes(16).toString('hex');
+    // // Tạo 1 token -> lưu lại -> gởi email + token -> email gởi lại token hợp lệ -> verified user
+    // const token = crypto.randomBytes(16).toString('hex');
 
-    await Token.create({
-      user: user._id,
-      email,
-      token: crypto.createHash('sha256').update(token).digest('hex'),
-      tokenExpire: Date.now() + 24 * 60 * 60 * 1000,
-    });
+    // await Token.create({
+    //   user: user._id,
+    //   email,
+    //   token: crypto.createHash('sha256').update(token).digest('hex'),
+    //   tokenExpire: Date.now() + 24 * 60 * 60 * 1000,
+    // });
 
-    // Send email
-    const tokenUrl = `<a href="${req.protocol}://${req.get(
-      'host',
-    )}/api/auth/confirmation/${token}">${req.protocol}://${req.get(
-      'host',
-    )}/api/auth/confirmation/${token}</a>`;
+    // // Send email
+    // const tokenUrl = `<a href="${req.protocol}://${req.get(
+    //   'host',
+    // )}/api/auth/confirmation/${token}">${req.protocol}://${req.get(
+    //   'host',
+    // )}/api/auth/confirmation/${token}</a>`;
 
-    const message = `<p>Hello ${user.fullName},</p><p>Bạn cần truy cập vào link sau để xác nhận tài khoản:</p><p>${tokenUrl}</p>`;
-    await sendEmail({
-      email: user.email,
-      subject: 'Account verification token',
-      message,
-    });
+    // const message = `<p>Hello ${user.fullName},</p><p>Bạn cần truy cập vào link sau để xác nhận tài khoản:</p><p>${tokenUrl}</p>`;
+    // await sendEmail({
+    //   email: user.email,
+    //   subject: 'Account verification token',
+    //   message,
+    // });
 
     return Response.success(res, { message: 'Tạo tài khoản thành công' });
   } catch (error) {
