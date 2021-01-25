d4315588b449096cf40b049b64f8e92ad0f7b386
unknown <duongthien2209@gmail.com>
2021-01-25 10:29:57 +0700

    xu ly hoa don -2

--- routes/user/bill.route.js
+++ routes/user/bill.route.js
@@ -7,6 +7,11 @@ const {
   getCTBill,
 } = require('../../controllers/bill.controller');
 
+// @route   GET api/user/bill/completed
+// @desc    Lấy danh sách hóa đơn đã thanh toán, đã hủy
+// @access  Private
+router.get('/completed', getAllByUserCompleted);
+
 // @route   GET api/user/bill
 // @desc    Lấy danh sách hóa đơn đang xử lý, đã xác nhận
 // @access  Private
@@ -16,10 +21,5 @@ router.get('/', getAllByUser);
 // @desc    Lấy chi tiết của 1 hóa đơn cụ thể
 // @access  Private
 router.get('/:billId', getCTBill);
-
-// @route   GET api/user/bill/completed
-// @desc    Lấy danh sách hóa đơn đã thanh toán, đã hủy
-// @access  Private
-router.get('/completed', getAllByUserCompleted);
 
 module.exports = router;
